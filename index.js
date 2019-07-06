77e7f09c3ebe432aa4b4b6d387d803fbc61938b4
David Gatti <github@gatti.email>
2019-07-06 11:14:24 +0200

    Switched support

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+let AWS = require('aws-sdk');
+
+//
+//  Create the DynamoDB object
+//
+let ddb = new AWS.DynamoDB.DocumentClient({
+    apiVersion: '2012-08-10',
+    region: process.env.AWS_REGION
+});
+
 //
 //  This function will grab the raw transcript of what was said in the
 //  message from AWS Lex, and pass it over to AWS Connect as text for
@@ -8,51 +18,88 @@ exports.handler = (event) => {
     return new Promise(function(resolve, reject) {
 
         //
-        //  1.  Set a default message so even if we get nothing back we have
-        //      a default message.
+        //  1.  Simplify the variable that we need.
         //
-        let message = "No message left";
+        //let name = event.Details.ContactData.Attributes.first_name
+        //let phone_nr = event.Details.ContactData.CustomerEndpoint.Address
 
-        //
-        //  2.  Check to see what did Lex hear from the user.
-        //
-        if(event.inputTranscript)
-        {
-            message = event.inputTranscript;
+        let container = {
+            //
+            //  Organize all the data that we get so we know what
+            //  this code uses.
+            //
+            req: {
+                phone_nr: 'dsdsds',
+                source: event.invocationSource
+            },
+            //
+            //  Save the transcript or set a default message.
+            //
+            message: event.inputTranscript || "No message left";
+            //
+            //  Hold the response for Connect.
+            //
+            res: {}
         }
 
         //
-        //  3.  Create a variable that will hold the response.
+        //  ->  Start the chain.
         //
-        let res = {};
+        process_the_transcript(container)
+            .then(function(container) {
+
+                return save_the_message(container);
+
+            }).catch(function(error) {
+
+            });
+
+    });
+
+};
+
+//   _____    _____     ____    __  __   _____    _____   ______    _____
+//  |  __ \  |  __ \   / __ \  |  \/  | |_   _|  / ____| |  ____|  / ____|
+//  | |__) | | |__) | | |  | | | \  / |   | |   | (___   | |__    | (___
+//  |  ___/  |  _  /  | |  | | | |\/| |   | |    \___ \  |  __|    \___ \
+//  | |      | | \ \  | |__| | | |  | |  _| |_   ____) | | |____   ____) |
+//  |_|      |_|  \_\  \____/  |_|  |_| |_____| |_____/  |______| |_____/
+//
+
+//
+//  See if we get back a transcript or not. And save the appropriate message.
+//
+function process_the_transcript(container) {
+
+    return new Promise(function(resolve, reject) {
 
         //
-        //  4.  This is ran from 'Lambda initialization and validation'.
+        //  1.  This is ran from 'Lambda initialization and validation'.
         //
-        if(event.invocationSource === 'DialogCodeHook')
+        if(container.req.source === 'DialogCodeHook')
         {
             //
             //  Set the Delegate with our modified slot value
             //
-            res = {
+            container.res = {
                 dialogAction: {
                     type: "Delegate",
                     slots: {
-                        message: message
+                        message: container.message
                     }
                 }
             };
         }
 
         //
-        //  This is ran from 'Fulfillment'.
+        //  2.  This is ran from 'Fulfillment'.
         //
-        if(event.invocationSource === 'FulfillmentCodeHook')
+        if(container.req.source === 'FulfillmentCodeHook')
         {
             //
             //  Close our intent.
             //
-            res = {
+            container.res = {
                 dialogAction: {
                     type: "Close",
                     fulfillmentState: "Fulfilled",
@@ -67,8 +114,52 @@ exports.handler = (event) => {
         //
         //  ->  Tell Lambda that we are done working.
         //
-        return resolve(res);
+        return resolve(container);
 
     });
 
+};
+
+
+//
+//  This lambda saves the name that the user over the phone said.
+//
+function save_the_message(container) {
+
+    return new Promise(function(resolve, reject) {
+
+        //
+        //  1.  Prepare the query
+        //
+        let params = {
+            TableName: "0x4447_connect_sessions",
+            Item: {
+                id: container.req.phone_nr,
+                type: 'message',
+                message: container.message
+            },
+        };
+
+        //
+        //  2.  Execute the query
+        //
+        ddb.put(params, function(error, data) {
+
+            //
+            //  1.  Check if there were any errors
+            //
+            if(error)
+            {
+                console.info(params);
+                return reject(error);
+            }
+
+            //
+            //  ->  Tell Lambda that we are done working.
+            //
+            return resolve(container);
+
+        });
+
+    });
 };
